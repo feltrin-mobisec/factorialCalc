b233f4ac72614538678bb68d029618b118dd86e3
Giacomo F <feltrin@mobisec.com>
2022-05-02 16:30:38 +0200

    Remove unuseful rows

--- app/controllers/factorials_controller.rb
+++ app/controllers/factorials_controller.rb
@@ -3,16 +3,8 @@ class FactorialsController < ApplicationController
 
   def calculate
     n = params[:number].to_i
-    if (n.is_a? Integer) && n > 0 && n <46
-      # i = 1
-      # while i < n
-      #   n = n * i
-      #   i += 1
-      # end
-      # render json: { "result": n}  
-      #n= (1..n).reduce(:*) || 1
-      #render json: { "result": n}  
-      
+    if (n.is_a? Integer) && n > 0 && n <46 
+
       i = 1
       while i < params[:number].to_i
         n = i * n
@@ -25,38 +17,6 @@ class FactorialsController < ApplicationController
       error(400, "The value is negative or not an integer")
     end
      
-  end
-
-
-
-  # def params
-  #   @number = params.require(:data)
-  # end
-
-  # def resp_body
-  #   {
-  #     factorial: @number.fact
-  #   }
-  # end
-
-
-
-  # attr_reader :number
-  # private :number
-
-  # def initialize(number)
-  #   @number = number
-  # end
-
-  # def get_result!
-  #   @number = @number.fact
-  #   return @number
-  # end
-
-  private
-  
-  def fact
-    (1..self).reduce(:*) || 1
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  # get 'number', to: "calculations#number"
-  # post "number", to: "calculations#number"
-  
-  
-  # get '/calculations/:id', to: 'calculations#show'
   get '/factorials/:number', to: 'factorials#calculate'
 
-
-
-  # scope '/api' do
-  #   scope '/v1' do
-  #     get '/factor_calc/number', to: 'factor_calc#number'
-  #   end
-  # end
 end
