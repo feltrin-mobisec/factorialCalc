e95c3387bfe1f73374e613246548aa5a93ce3499
Giacomo F <feltrin@mobisec.com>
2022-05-03 18:09:00 +0200

    Implement json request

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   get '/factorials/:number', to: 'factorials#calculate', constraints: { number: /[^\/]+/ }, :constraints => { number: /.*/}
+  get '/json_factorials', to: 'json_factorials#calculate'
 
 end
